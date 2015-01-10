d5c01cd1811577f43566c6c98b4d820ff3e93651
chriszeng8 <chriszeng8@hotmail.com>
2015-01-10 00:18:34 -0500

    extract xml values from html element, using espn Baltimore Ravens as an example

---
+++ Reading_File_Diff_Type/Reading_html.r
@@ -0,0 +1,23 @@
+# Jan 10, 2015 Zeng
+# Here we are reading and parsing a webpage (i.e., html), and
+# extract the xmlValues of specificied elements/classes using
+# ============================================================
+# Interest<-xpathSApply(Parsed_doc,"//li[@class='']",xmlValue)
+# ============================================================
+
+fileUrl<-"http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+
+# ========================  Important ========================
+# "useInternalNodes = TRUE" will enable us to record all the 
+# sub-node within the top level nodes
+doc<-htmlTreeParse(fileUrl,useInternalNodes = TRUE)
+
+# Extract xmlValues from the list class score
+# <li class="score"> 
+scores<-xpathSApply(doc,"//li[@class='score']",xmlValue)
+teams<-xpathSApply(doc,"//li[@class='team-name']",xmlValue)
+game_status<-xpathSApply(doc,"//li[@class='game-status']",xmlValue)
+
+scores
+teams
+game_status
